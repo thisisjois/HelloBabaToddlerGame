315ffcb25f729c772c5bb9f80d780ffc82a4af86
jois <thisisjois@gmail.com>
2022-04-27 18:32:13 +0700

    Initial Commit

---
+++ HelloBabaToddlerGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloBabaToddlerGame
+//
+//  Created by Nathania Joyce on 27/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloBabaToddlerGame/HelloBabaToddlerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBabaToddlerGameApp.swift
+//  HelloBabaToddlerGame
+//
+//  Created by Nathania Joyce on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloBabaToddlerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
